3d83e074112dd5e53c7858450f66e6a1abd815d4
Marcin Bach <37640343+bachus03@users.noreply.github.com>
2024-03-05 14:02:41 +0100

    Update NumberApi.php

--- Infobip/Api/NumberApi.php
+++ Infobip/Api/NumberApi.php
@@ -474,7 +474,7 @@ class NumberApi
 
         $query = \GuzzleHttp\Psr7\Query::build($queryParams);
         return new Request(
-            'POST',
+            'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
             $headers,
             $httpBody
